3cf729a9ad1eb40997213cdeef23972f045a475c
Raghavendrajoshi45 <74344432+Raghavendrajoshi45@users.noreply.github.com>
2025-10-04 20:22:45 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
   public static void main(String[] args) {
     // two integer variables with values
     // and a variable "sum" to store the result
-    int num1 = 20, num2 = 15,sum;
+    int num1 = 20, num2 = 15,sum,mul;
 
     //calculating the sum of num1 and num2 and
     //storing the result in the variable sum
